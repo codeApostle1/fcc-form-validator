0d9486ae3f4ca0039d48b5de9f4e7794fc6da841
J-max <joelmtn7@gmail.com>
2025-02-17 22:30:23 +0100

    ok

---
+++ script.js
@@ -0,0 +1,24 @@
+const calorieCounter = document.getElementById('calorie-counter'); 
+// for the form
+
+const budgetNumberInput = document.getElementById('budget');
+// for the budget top input
+
+const entryDropdown = document.getElementById('entry-dropdown');
+// for thr dropdown below the form
+
+const addEntryButton = document.getElementById('add-entry');
+// for the add entry button
+
+const clearButton = document.getElementById('clear');
+// for the clear button
+
+const output = document.getElementById('output');
+// for the output below the clear button
+
+let isError = false;
+
+
+function cleanInputString(str){
+    const regex = /\+-\s/;
+}
